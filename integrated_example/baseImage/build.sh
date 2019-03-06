576b022275bfa926c9a7d02beeb2378f7d70b602
Johan van Soest <johan.vansoest@maastro.nl>
2019-03-06 08:40:53 +0100

    Splitted building base container (always the same pip libraries) from the code which actually changes. Speeds up development

---
+++ integrated_example/baseImage/build.sh
@@ -0,0 +1,7 @@
+docker build --no-cache -t fairhealth/basetrain ./
+
+printf "Upload to Docker Hub?\nOptions [Y/n]: "
+read uploadChoice
+if [ $uploadChoice != "n"]; then
+    docker push fairhealth/basetrain
+fi
--- integrated_example/build.sh
+++ integrated_example/build.sh
@@ -1,7 +1,8 @@
+docker rmi fairhealth/ppdml
 docker build --no-cache -t fairhealth/ppdml ./
 
 printf "Upload to Docker Hub?\nOptions [Y/n]: "
 read uploadChoice
-if [ $uploadChoice != "n"]; then
+if [ $uploadChoice != "n" ]; then
     docker push fairhealth/ppdml
 fi
